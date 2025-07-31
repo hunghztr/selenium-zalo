ddbe092f51cd0d9605b12a1acd576ced2d5292a8
hunghztr <tranhunghat141512@gmail.com>
2025-07-31 17:08:21 +0700

    final

--- open_zalo.py
+++ open_zalo.py
@@ -11,7 +11,7 @@ def open_zalo():
     debug_port = "--remote-debugging-port=9222"
     user_data_dir = r'--user-data-dir=C:/Chrome_dev'
     subprocess.Popen([chrome_path, debug_port, user_data_dir])
-    time.sleep(0.5)  # Thời gian chờ để Chrome khởi động
+    time.sleep(5)  # Thời gian chờ để Chrome khởi động
     # Thiết lập Chrome Options để kết nối tới phiên Chrome đang mở
     chrome_options = Options()
     chrome_options.add_experimental_option("debuggerAddress", "127.0.0.1:9222")
@@ -25,7 +25,7 @@ def open_zalo():
     print("Đang mở trang web...")
     driver.execute_script("window.open('https://chat.zalo.me/', '_blank');")
     driver.switch_to.window(driver.window_handles[-1])
-    time.sleep(0.5)  # Chờ thêm để trang tải đầy đủ
+    time.sleep(3)  # Chờ thêm để trang tải đầy đủ
     return driver,wait
   except Exception as e:
     print(f"Lỗi khi mở trang web: {e}")
